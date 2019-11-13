f6588c758a80827b8f7d47be162ddb3e18ce1697
Amnon Ophir <ophiramnon@gmail.com>
2019-11-13 19:11:14 +0200

    Adding getting the expression from cmd

--- Son/main.c
+++ Son/main.c
@@ -49,27 +49,35 @@ int evaluate_simple_expression(char expression[])
     char delimiter[] = "+*";
     int first_operand, second_operand = 0;
     char operator;
+	char* next_token = NULL;
 
     operator = expression[get_operator_index(expression)];
 
     char *first_operand_string;
-    first_operand_string = strtok(expression, delimiter);
+    first_operand_string = strtok_s(expression, delimiter, &next_token);
     first_operand = parse_number(first_operand_string);
 
     char *second_operand_string;
-    second_operand_string = strtok(NULL, delimiter);
+    second_operand_string = strtok_s(NULL, delimiter, &next_token);
     second_operand = parse_number(second_operand_string);
 
     return calculate(first_operand, second_operand, operator);
 }
 
-int main()
+int main(int argc, char** argv)
 {
-    char expression[32] = "1000*1000";
+	if (argc < 2)
+	{
+		printf("Not enough arguments.");
+		return -1;
+	}
+
+	char* expression;
     int result;
+	expression = argv[1];
     result = evaluate_simple_expression(expression);
 
-    printf("%d", result);
+    printf("%d\n", result);
 
     return 0;
 }
