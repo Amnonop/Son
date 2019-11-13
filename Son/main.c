55cb9ac64d01d198f3e7ef6955415e7f82c22ae4
Amnon Ophir <ophiramnon@gmail.com>
2019-11-13 18:45:17 +0200

    Initial commit of solution

---
+++ Son/main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int add(int first_operand, int second_operand)
+{
+    return first_operand + second_operand;
+}
+
+int multiply(int first_operand, int second_operand)
+{
+    return first_operand * second_operand;
+}
+
+int calculate(int first_operand, int second_operand, char operator)
+{
+    if (operator == '+')
+    {
+        return add(first_operand, second_operand);
+    }
+    else if (operator == '*')
+    {
+        return multiply(first_operand, second_operand);
+    }
+}
+
+int parse_number(char string_to_parse[])
+{
+    int number;
+    number = (int)strtol(string_to_parse, (char **)NULL, 10);
+
+    return number;
+}
+
+int get_operator_index(char expression[])
+{
+    int i = 0;
+
+    while ((expression[i] != '+') && (expression[i] != '*'))
+    {
+        i++;
+    }
+
+    return i;
+}
+
+int evaluate_simple_expression(char expression[])
+{
+    char delimiter[] = "+*";
+    int first_operand, second_operand = 0;
+    char operator;
+
+    operator = expression[get_operator_index(expression)];
+
+    char *first_operand_string;
+    first_operand_string = strtok(expression, delimiter);
+    first_operand = parse_number(first_operand_string);
+
+    char *second_operand_string;
+    second_operand_string = strtok(NULL, delimiter);
+    second_operand = parse_number(second_operand_string);
+
+    return calculate(first_operand, second_operand, operator);
+}
+
+int main()
+{
+    char expression[32] = "1000*1000";
+    int result;
+    result = evaluate_simple_expression(expression);
+
+    printf("%d", result);
+
+    return 0;
+}
